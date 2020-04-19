a0be28cadb59f54c94930c15d4313accbf82e4a2
Tristan Maindron <tmaindron@gmail.com>
2020-04-19 04:45:35 +0200

    Added jump of a cliff

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -7,9 +7,18 @@ public class Hero : MonoBehaviour
 {
     const float speed = 150f;
 
+    const float jumpUpForce = 200f;
+    const float jumpSideForce = 70f;
+
+    const float groundedRaycastDistance = 0.4f;
+
     public bool IsMoving = false;
     public Vector2 move;
     
+    public Vector2 lastMovement; // Last non null movement
+
+    bool grounded = true;
+
     void Start()
     {
         
@@ -19,14 +28,31 @@ public class Hero : MonoBehaviour
     {
         //IsMoving = true;
         move = value.Get<Vector2>();
+
+        if (move.x != 0 || move.y != 0) {
+            lastMovement = move.normalized;
+        }
     }
 
     void FixedUpdate()
     {
+        bool grounded = Physics.Raycast(transform.position, Vector3.down, 0.3f, LayerMask.GetMask("Ground"));
+
+        if (grounded == false && this.grounded)
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            GetComponent<Rigidbody>().AddForce(new Vector3(lastMovement.x * jumpSideForce, jumpUpForce, lastMovement.y * jumpSideForce));
+        }
+
+        this.grounded = grounded;
+
+
         GetComponent<SpriteRenderer>().sortingOrder = (int) -(transform.position.z * 1000);
 
-        Vector3 currentVelocity = GetComponent<Rigidbody>().velocity;
-        GetComponent<Rigidbody>().velocity = new Vector3(0f, currentVelocity.y, 0f) + new Vector3(move.x, 0f, move.y) * Time.fixedDeltaTime * speed;
+        if (grounded) {
+            Vector3 currentVelocity = GetComponent<Rigidbody>().velocity;
+            GetComponent<Rigidbody>().velocity = new Vector3(0f, currentVelocity.y, 0f) + new Vector3(move.x, 0f, move.y) * Time.fixedDeltaTime * speed;
+        }
     }
     private void LateUpdate()
     {
