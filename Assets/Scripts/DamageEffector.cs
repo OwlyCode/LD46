15f8b8571d9aae1e1b37c6bef5cd241837034d36
Tristan Maindron <tmaindron@gmail.com>
2020-04-21 01:48:31 +0200

    Added destruction effect

---
+++ Assets/Scripts/DamageEffector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageEffector : MonoBehaviour
+{
+    public GameObject breakParticles;
+
+    void OnDisable()
+    {
+        Instantiate(breakParticles, transform.position, Quaternion.identity);
+    }
+}
